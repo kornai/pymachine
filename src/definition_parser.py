7f0077f397d6ba3645a32de5154352958a17f03c
Attila Zseder <zseder@ilab.sztaki.hu>
2011-08-19 18:44:37 +0200

    function that creates machines from parse tree reimplemented not done yet, but the idea is trivial, just have to finish there is one question in the code raising a NotImplementedError, works as a todo (or tothinkabout)

--- src/definition_parser.py
+++ src/definition_parser.py
@@ -15,22 +15,35 @@ class DefinitionParser:
                    , "ILL", "INE", "ELA"         # IN
                    , "ALL", "ADE", "ABL"         # AT
                    ]
+
+    lb = "["
+    rb = "]"
+    lp = "("
+    rp = ")"
+
+    def_sep = ":"
+    arg_sep = ","
+    part_sep = ";"
+    comment_sep = "%"
+    prime = "'"
+
     def __init__(self):
-        
+        self.init_constants()
         self.init_parser()
+
     
     def init_parser(self):
         self.id = Word(nums)
-        self.lb = Literal("[")
-        self.rb = Literal("]")
-        self.lp = Literal("(")
-        self.rp = Literal(")")
+        self.lb_lit = Literal(DefinitionParser.lb)
+        self.rb_lit = Literal(DefinitionParser.rb)
+        self.lp_lit = Literal(DefinitionParser.lp)
+        self.rp_lit = Literal(DefinitionParser.rp)
 
-        self.def_sep = Literal(":")
-        self.arg_sep = Literal(",")
-        self.part_sep = Literal(";")
-        self.comment_sep = Literal("%")
-        self.prime = Literal("'")
+        self.def_sep_lit = Literal(DefinitionParser.def_sep)
+        self.arg_sep_lit = Literal(DefinitionParser.arg_sep)
+        self.part_sep_lit = Literal(DefinitionParser.part_sep)
+        self.comment_sep_lit = Literal(DefinitionParser.comment_sep)
+        self.prime_lit = Literal(DefinitionParser.prime)
         
         #self.deep_cases = (Literal("NOM") | Literal("ACC") | Literal("DAT") | Literal("INS") | Literal("ABL"))
         self.deep_cases = reduce(lambda a, b: a | b, (Literal(dc) for dc in DefinitionParser._deep_cases))
@@ -42,16 +55,15 @@ class DefinitionParser:
         # main expression
         self.expression = Forward()
         
-        # "enumerable expression
+        # "enumerable expression"
         # D -> E | E, D
-        self.definition = (self.expression + Optional(OneOrMore(self.arg_sep.suppress() + self.expression)))
-        
+        self.definition = (self.expression + Optional(OneOrMore(self.arg_sep_lit.suppress() + self.expression)))
         self.expression << Group(
                             # E -> U [ D ]
-                            (self.unary + self.lb.suppress() + Group(self.definition) + self.rb.suppress() ) ^ 
+                            (self.unary + self.lb_lit.suppress() + Group(self.definition) + self.rb_lit.suppress() ) ^ 
 
                             # E -> U ( U ) | U ( U [ E ] )
-                            (self.unary + self.lp + self.unary + Optional(self.lb + self.expression + self.rb) + self.rp ) ^
+                            (self.unary + self.lp_lit + self.unary + Optional(self.lb_lit + self.expression + self.rb_lit) + self.rp_lit ) ^
 
                             # E -> U B E
                             (self.unary + self.binary + self.expression) ^
@@ -63,32 +75,32 @@ class DefinitionParser:
                             (self.unary) ^
 
                             # E -> B [ E ; E ] 
-                            (self.binary + self.lb.suppress() + Group(self.expression) + self.part_sep.suppress() + Group(self.expression) + self.rb.suppress()) ^
+                            (self.binary + self.lb_lit.suppress() + Group(self.expression) + self.part_sep_lit.suppress() + Group(self.expression) + self.rb_lit.suppress()) ^
                             
                             # E -> [ E ] B [ E ]
-                            (self.lb.suppress() + Group(self.expression) + self.rb.suppress() + self.binary + self.lb.suppress() + Group(self.expression) + self.rb.suppress()) ^
+                            (self.lb_lit.suppress() + Group(self.expression) + self.rb_lit.suppress() + self.binary + self.lb_lit.suppress() + Group(self.expression) + self.rb_lit.suppress()) ^
                             
                             # E -> B [ E ]
-                            (self.binary + self.lb.suppress() + Group(self.expression) + self.rb.suppress()) ^
+                            (self.binary + self.lb_lit.suppress() + Group(self.expression) + self.rb_lit.suppress()) ^
                             
                             # E -> [ E ] B
-                            (self.lb.suppress() + Group(self.expression) + self.rb.suppress() + self.binary) ^
+                            (self.lb_lit.suppress() + Group(self.expression) + self.rb_lit.suppress() + self.binary) ^
                             
                             # E -> B E
                             (self.binary + self.expression) ^
 
                             # E -> 'B
-                            (self.prime + self.binary) ^
+                            (self.prime_lit + self.binary) ^
 
                             # E -> B'
-                            (self.binary + self.prime)
+                            (self.binary + self.prime_lit)
                            )
         
         self.hu, self.pos, self.en, self.lt, self.pt = (Word(alphanums + "#-/_" ),) * 5
         self.word = Group(self.hu + self.pos + self.en + self.lt + self.pt)
 
         # S -> W : D | W : D % _
-        self.sen = (self.id + self.word + self.def_sep.suppress() + self.definition + Optional(self.comment_sep + self.dontcare).suppress()) + LineEnd()
+        self.sen = (self.id + self.word + self.def_sep_lit.suppress() + self.definition + Optional(self.comment_sep_lit + self.dontcare).suppress()) + LineEnd()
     
     def parse(self, s):
         return self.sen.parseString(s).asList()
@@ -197,6 +209,129 @@ class DefinitionParser:
                 return m
             else:
                 raise ParserException("there shouldn't be 4 nodes in a tree only if 2 of them are parentheses")
+
+    def __parse_expr_v2(cls, expr):
+        """
+        creates machines from a parse node and its children
+        there should be one handler for every rule
+        """
+        raise NotImplementedError("kell-e a sok expression ele1 a Group() a szaba1lyokna1l az initne1l? Kezdetben volt e1rtelme, de ma1r nem emle1kszem, hogy mi. Lehet to2ro2lheto3.") 
+        is_binary = cls._is_binary
+        is_unary = cls._is_unary
+        is_expr = lambda r: type(r) == list
+
+        # E -> U [ D ]
+        # ['unary', [['unary'], ['unary']]]
+        if (len(expr) == 2 and
+              is_unary(expr[0]) and
+              is_expr(expr[1])):
+            m = Machine(Monoid(expr[0]))
+            for _property in expr[1]:
+                m.append(1, cls.__parse_expr_v2(_property))
+            return m
+
+        # E -> U ( U ) | U ( U [ E ] )
+        # ['unary', '(', 'unary', ')']
+        if (len(expr) == 4 and
+              is_unary(expr[0]) and
+              expr[1] == cls.lp and
+              is_expr(expr[2]) and
+              expr[3] == cls.rp):
+            m = Machine(Monoid(expr[0]))
+            m.append(1, Machine(Monoid(expr[2])))
+            return m
+        # ???
+
+        # E -> U B E
+        # ['unary', 'BINARY', ['unary']]
+        if (len(expr) == 3 and
+              is_unary(expr[0]) and
+              is_binary(expr[1]) and
+              is_expr(expr[2])):
+            m = Machine(Monoid(expr[1]))
+            m.append(1, Machine(Monoid(expr[0])))
+            m.append(2, cls.__parse_expr_v2(expr[2]))
+            return m
+
+        # E -> U B
+        # ['unary', 'BINARY']
+        if (len(expr) == 2 and
+              is_unary(expr[0]) and
+              is_binary(expr[1])):
+            m = Machine(Monoid(expr[1]))
+            m.append(1, Machine(Monoid(expr[0])))
+            m.base.partitions.append([])
+
+        # E -> U
+        # ['unary']
+        if (len(expr) == 1 and
+              is_unary(expr[0])):
+            return Machine(Monoid(expr[0]))
+
+        # E -> B [ E ; E ] 
+        # ['BINARY', [['unary']], [['unary']]]
+        if (len(expr) == 3 and
+              is_binary(expr[0]) and
+              is_expr(expr[1]) and
+              is_expr(expr[2])):
+            m = Machine(Monoid(expr[0]))
+            m.append(1, cls.__parse_expr_v2(expr[1]))
+            m.append(2, cls.__parse_expr_v2(expr[2]))
+            return m
+
+        # E -> [ E ] B [ E ]
+        # [[['unary']], 'BINARY', [['unary']]]
+        if (len(expr) == 3 and
+              is_expr(expr[0]) and
+              is_binary(expr[1]) and
+              is_expr(expr[1])):
+            m = Machine(Monoid(expr[1]))
+            m.append(1, cls.__parse_expr_v2(expr[0]))
+            m.append(2, cls.__parse_expr_v2(expr[2]))
+            return m
+            
+
+        # E -> B [ E ]
+        # ['BINARY', [['unary']]]
+        if (len(expr) == 2 and
+            is_binary(expr[0]) and
+            is_expr(expr[1])):
+            m = Machine(Monoid(expr[0]))
+            m.append(2, cls.__parse_expr_v2(expr[1]))
+            return m
+
+        # E -> [ E ] B
+        # [[['unary']], 'BINARY']
+        if (len(expr) == 2 and
+            is_expr(expr[0]) and
+            is_binary(expr[1])):
+            m = Machine(Monoid(expr[1]))
+            m.append(1, cls.__parse_expr_v2(expr[0]))
+            m.base.partitions.append([])
+            return m
+
+        # E -> B E
+        # ['BINARY', ['unary']]
+        if (len(expr) == 2 and
+            is_binary(expr[0]) and
+            is_expr(expr[1])):
+            m = Machine(Monoid(expr[0]))
+            m.append(2, cls.__parse_expr_v2(expr[1]))
+            return m
+
+        # E -> 'B
+        # ["'", 'BINARY']
+        if (len(expr) == 2 and
+            expr[0] == cls.prime and
+            is_binary(expr[1])):
+            raise NotImplementedError()
+
+        # E -> B'
+        # ['BINARY', "'"]
+        if (len(expr) == 2 and
+            is_binary(expr[0]) and
+            expr[1] == cls.prime):
+            raise NotImplementedError()
     
     def parse_into_machines(self, s):
         parsed = self.parse(s)
