862eb7e7319b759afe5252abcdcaa9f1978a820e
Attila Zseder <zseder@ilab.sztaki.hu>
2011-09-06 15:57:22 +0200

    changed ordering of rules (maybe useless) removed Optional from one rule and created two different rules from that (for using Optional(), deeper understanding of pyparsing will be required)

--- src/definition_parser.py
+++ src/definition_parser.py
@@ -60,36 +60,39 @@ class DefinitionParser:
         # D -> E | E, D
         self.definition = Group(delimitedList(self.expression, delim=DefinitionParser.arg_sep))
         self.expression << Group(
+                            # E -> U
+                            (self.unary) ^
+
                             # E -> U [ D ]
                             (self.unary + self.lb_lit.suppress() + self.definition + self.rb_lit.suppress() ) ^ 
 
                             # E -> U ( U ) | U ( U [ E ] )
                             (self.unary + self.lp_lit + self.unary + Optional(self.lb_lit + self.expression + self.rb_lit) + self.rp_lit ) ^
 
-                            # E -> U B E
-                            (self.unary + self.binary + self.expression) ^
-
                             # E -> U B
                             (self.unary + self.binary) ^
                             
-                            # E -> U
-                            (self.unary) ^
+                            # E -> U B E
+                            (self.unary + self.binary + self.expression) ^
 
-                            # E -> B [ E ; E ] 
-                            (self.binary + self.lb_lit.suppress() + self.expression + self.part_sep_lit.suppress() + self.expression + self.rb_lit.suppress()) ^
-                            
-                            # E -> [ E ] B [ E ] | [ E ] B E
-                            (self.lb_lit.suppress() + self.expression + self.rb_lit.suppress() + self.binary + Optional(self.lb_lit.suppress()) + self.expression + Optional(self.rb_lit.suppress())) ^
-                            
+                            # E -> B E
+                            (self.binary + self.expression) ^
+
                             # E -> B [ E ]
                             (self.binary + self.lb_lit.suppress() + self.expression + self.rb_lit.suppress()) ^
+                            
+                            # E -> B [ E ; E ] 
+                            (self.binary + self.lb_lit.suppress() + self.expression + self.part_sep_lit.suppress() + self.expression + self.rb_lit.suppress()) ^
                             
                             # E -> [ E ] B
                             (self.lb_lit.suppress() + self.expression + self.rb_lit.suppress() + self.binary) ^
                             
-                            # E -> B E
-                            (self.binary + self.expression) ^
-
+                            # E -> [ E ] B [ E ]
+                            (self.lb_lit.suppress() + self.expression + self.rb_lit.suppress() + self.binary + self.lb_lit.suppress() + self.expression + self.rb_lit.suppress()) ^
+                            
+                            # E -> [ E ] B E
+                            (self.lb_lit.suppress() + self.expression + self.rb_lit.suppress() + self.binary + self.expression ) ^
+                            
                             # E -> 'B
                             (self.prime_lit + self.binary) ^
 
