1ab8dd8f7cf776bb2e54056f009857f2810b3677
Attila Zseder <zseder@ilab.sztaki.hu>
2011-09-13 16:15:16 +0200

    added some exceptions to machine_exceptions

--- src/machine_exceptions.py
+++ src/machine_exceptions.py
@@ -4,3 +4,14 @@ class UnknownWordException(Exception):
 class UnknownSentenceException(Exception):
     pass
 
+class TooManyArgumentsException(Exception):
+    def __init__(self, m):
+        self._machines = m
+
+class CaseAmbiguityException(Exception):
+    def __init__(self, m):
+        self._ambiguate_machines = m
+
+class LocationAmbiguityException(Exception):
+    def __init__(self, m):
+        self._ambiguate_machines = m
