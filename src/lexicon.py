0e54a8d5e8bce4dda06484c513c71d11d117e410
Attila Zseder <zseder@ilab.sztaki.hu>
2012-01-17 12:01:15 +0100

    added empty methods to be implemented

--- src/lexicon.py
+++ src/lexicon.py
@@ -1,8 +1,22 @@
+from constants import deep_cases
+
 class Lexicon:
     """THE machine repository."""
     def __init__(self):
-        self.deep_cases = set([])  # Set of deep cases
-        pass
+        self.deep_cases = set(deep_cases)  # Set of deep cases
+
+    def add_active(self, machines):
+        """
+        adds machines to active collection
+        typically called to add a sentence being worked with
+        """
+
+    def add_static(self, machines):
+        """
+        adds machines to static collection
+        typically called once to add whole background knowledge
+        which is the input of the definition parser
+        """
 
     def activate(self, already_active):
         """Finds and returns the machines that should be activated by the
