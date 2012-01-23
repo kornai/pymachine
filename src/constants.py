57875ec944dbce0ab86265817907dbbcaf7eac42
David Nemeskey <ndavid@ilab.sztaki.hu>
2012-01-23 17:46:52 +0100

    Fixed erroneous locative case name.

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
                  # TO     AT     FROM
-locative_cases = ["SUL", "SUE", "DEL", # ON
+locative_cases = ["SBL", "SUE", "DEL", # ON
                   "ILL", "INE", "ELA", # IN
                   "ALL", "ADE", "ABL"] # AT
 deep_cases = locative_cases + ["NOM", "ACC", "DAT", "INS", "OBL"]
