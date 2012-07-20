1d7451ad5c6e9cec5d06d48cd9b368e0682e4603
David Nemeskey <nemeskey.david@sztaki.hu>
2012-07-20 16:56:53 +0200

    Added a construction step to spreading activation.

--- src/spreading_activation.py
+++ src/spreading_activation.py
@@ -1,5 +1,6 @@
 from control import PluginControl
 import logging
+import itertools
 
 class SpreadingActivation(object):
     """Implements spreading activation (surprise surprise)."""
@@ -18,6 +19,7 @@ class SpreadingActivation(object):
         # TODO: NPs/ linkers to be contended
         last_active = len(self.lexicon.active)
         unexpanded = list(self.lexicon.get_unexpanded())
+        sentence = set(unexpanded)
         linking = {}  # {linker: set([machines that have the linker on a partition])}
 
         # This condition will not work w/ the full lexicon, obviously.
@@ -29,7 +31,7 @@ class SpreadingActivation(object):
 #            for ac in self.lexicon.active.values():
 #                for m in ac:
 #                    logging.debug(Machine.to_debug_str(m))
-            # Step 1
+            # Step 1: expansion
             for machine in unexpanded:
                 self.lexicon.expand(machine)
                 for partition in machine.base.partitions[1:]:
@@ -42,7 +44,40 @@ class SpreadingActivation(object):
                             linking[submachine] = s
             # XXX: activate Elvira here?
 
-            # Step 2
+            # Step 2b: constructions:
+            for c in self.lexicon.constructions:
+                accepted = []
+                # Find the sequences that match the construction
+                # TODO: combinatorial explosion alert!
+                for elems in xrange(len(sentence)):
+                    for seq in itertools.permutations(sentence, elems + 1):
+                        if c.check(seq):
+                            accepted.append(seq)
+
+                # The sequence preference order is longer first
+                # TODO: obviously this won't work for every imaginable
+                #       combination; maybe this step should be combination-
+                #       dependent.
+                accepted.sort(key=lambda seq: len(seq))
+
+                # No we try to act() on these sequences. We stop at the first
+                # sequence that is accepted by act().
+                while len(accepted) > 0:
+                    seq = accepted[-1]
+                    c_res = c.act(seq)
+                    if c_res is not None:
+                        # We remove the machines that were consumed by the
+                        # construction and add the machines returned by it
+                        for m in seq:
+                            sentence.remove(m)
+                        for m in c_res:
+                            sentence.add(m)
+                        break
+                    else:
+                        del accepted[-1]
+
+            # Step 2: linking
+            # TODO: replace w/ constructions
             # XXX: What if there are more than 2 of the same linker?
             linker_to_remove = []
             for linker, machines in linking.iteritems():
@@ -52,7 +87,7 @@ class SpreadingActivation(object):
             for linker in linker_to_remove:
                 del linking[linker]
 
-            # Step 3
+            # Step 3: activation
             self.lexicon.activate()
             unexpanded = list(self.lexicon.get_unexpanded())
             if len(self.lexicon.active) == last_active:
@@ -64,6 +99,8 @@ class SpreadingActivation(object):
         ret = []
         for m in self.lexicon.get_expanded():
             if isinstance(m.control, PluginControl):
+                # TODO: rename to activate() or sth and get rid of the
+                # call to isinstance()
                 msg = m.control.message()
                 if msg is not None:
                     ret.append(msg)
