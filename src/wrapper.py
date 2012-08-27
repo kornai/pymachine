c9ab4d17d2530f7f9bf156b22fd7c65a427c9dda
Attila Zseder <zseder@ilab.sztaki.hu>
2012-08-27 12:56:53 +0200

    modified:   ../pymachine/src/wrapper.py - sup_dic reading - PutConstruction is added by hand as in elvira case

--- src/wrapper.py
+++ src/wrapper.py
@@ -5,7 +5,9 @@ from sentence_parser import SentenceParser
 from lexicon import Lexicon
 from spreading_activation import SpreadingActivation
 from definition_parser import read as read_defs
-from construction import VerbConstruction, ElviraConstruction
+from construction import (VerbConstruction, ElviraConstruction,
+                          TheConstruction, DummyNPConstruction)
+from sup_dic import supplementary_dictionary_reader as sdreader
 
 config_filename = "machine.cfg"
 
@@ -23,22 +25,41 @@ class Wrapper:
         config.read(self.cfn)
         items = dict(config.items("machine"))
         self.def_fn = items["definitions"]
+        self.supp_dict_fn = items["supp_dict"]
 
     def __read_files(self):
         self.__read_definitions()
+        self.__read_supp_dict()
 
     def __read_definitions(self):
         definitions = read_defs(file(self.def_fn))
         self.lexicon.add_static(definitions.itervalues())
 
+    def __read_supp_dict(self):
+        self.supp_dict = sdreader(file(self.supp_dict_fn))
+
     def __add_constructions(self):
         # TODO this should be created automatically, maybe from knowing,
         # that "megy" is actually a verb, there is a POS in the definitions
-        megy_construction = VerbConstruction("megy", self.lexicon.static[
-            "megy"])
-        del self.lexicon.static["megy"]
-        self.lexicon.add_construction(megy_construction)
+        try:
+            megy_construction = VerbConstruction("megy", self.lexicon.static[
+                "megy"], self.supp_dict)
+            del self.lexicon.static["megy"]
+            self.lexicon.add_construction(megy_construction)
+        except KeyError:
+            pass
+        try:
+            tesz_construction = VerbConstruction("tesz", self.lexicon.static[
+                "tesz"], self.supp_dict)
+            del self.lexicon.static["tesz"]
+            self.lexicon.add_construction(tesz_construction)
+        except KeyError:
+            pass
         self.lexicon.add_construction(ElviraConstruction())
+
+        self.lexicon.add_construction(DummyNPConstruction())
+        self.lexicon.add_construction(TheConstruction())
+        
 
         # TODO create shrdlu construction
 
