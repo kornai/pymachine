511c2c42034237d928454590353e87d2ae87d95e
Attila Zseder <zseder@ilab.sztaki.hu>
2012-09-19 13:17:34 +0200

    modified:   wrapper.py wrapper.py now accepts definition file in machine.cfg line a comma seperated list of file names, and puts all the machines into static

--- src/wrapper.py
+++ src/wrapper.py
@@ -9,10 +9,8 @@ from matcher import *
 from sup_dic import supplementary_dictionary_reader as sdreader
 from avm import AVM
 
-config_filename = "machine.cfg"
-
 class Wrapper:
-    def __init__(self, cf=config_filename):
+    def __init__(self, cf):
         self.cfn = cf
         self.__read_config()
 
@@ -27,7 +25,7 @@ class Wrapper:
         config = ConfigParser.SafeConfigParser({"machinepath": machinepath})
         config.read(self.cfn)
         items = dict(config.items("machine"))
-        self.def_fn = items["definitions"]
+        self.def_files = [s.strip() for s in items["definitions"].split(",")]
         self.supp_dict_fn = items["supp_dict"]
 
     def __read_files(self):
@@ -35,8 +33,9 @@ class Wrapper:
         self.__read_supp_dict()
 
     def __read_definitions(self):
-        definitions = read_defs(file(self.def_fn))
-        self.lexicon.add_static(definitions.itervalues())
+        for f in self.def_files:
+            definitions = read_defs(file(f))
+            self.lexicon.add_static(definitions.itervalues())
 
     def __read_supp_dict(self):
         self.supp_dict = sdreader(file(self.supp_dict_fn))
