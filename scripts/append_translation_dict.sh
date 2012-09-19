063eaac24a1f9af658b74356a8970ecd5552edf4
Attila Zseder <zseder@ilab.sztaki.hu>
2012-09-19 16:43:48 +0200

    modified:   ../scripts/append_translation_dict.sh - now this script needs 3 arguments, and the third is output, so input   files are not modified. This is good because we don't want to modify   files that are part of git

--- scripts/append_translation_dict.sh
+++ scripts/append_translation_dict.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 concept_definitions=$1
 langspec_definitions=$2
-cut -f1,2,4 -d" " $concept_definitions | grep -v "^[[:space:]]*$" | awk '{print $1,$2,"N","#","#","#: ", $3}' - >> $langspec_definitions
+out=$3
+cp $langspec_definitions $out
+cut -f1,2,4 -d" " $concept_definitions | grep -v "^[[:space:]]*$" | awk '{print $1,$2,"N","#","#","#: ", $3}' - >> $out
