e90a87ce3bd81b2c92504f2ef391aed0fb83d88e
Gabor Recski <recski@sztaki.hu>
2012-11-21 15:12:56 +0100

    find_next_kr replaced with a simple split()

--- src/np_parser.py
+++ src/np_parser.py
@@ -2,40 +2,15 @@ import sys
 from langtools.utils import readkr
 import matcher
 
-
-def find_next_kr(string):
-    
-    first = len(string)
-    last = first 
-    for index, char in enumerate(string):
-        if char !=' ':
-           first = index
-           break
-    last_char = '@'
-    current = first
-    for index, char in enumerate(string[first:]):
-        if char.isupper() and (last_char == ')' or last_char == '>'):
-            last = current
-            break      
-        elif char != ' ':
-            last_char = char
-            current = index
-    return string[first:first + last + 1], string[first + last + 1:] 
-
-   
 def parse_rule(rule):
-    right = rule.split('->')[1]
+    right = rule.split('->')[1].strip()
     matchers = []
-    while len(right) > 0:
-        kr, right = find_next_kr(right)
-        if kr != '':
+    for kr in right.split():        
             kr_dict = readkr.kr_to_dictionary('stem/' + kr)
             pattermatch = matcher.PatternMatcher(kr_dict)
-        matchers.append(pattermatch)
+            matchers.append(pattermatch)
     return matchers
 
-     
-        
 def main():
     rules = sys.stdin.readlines()
     for rule in rules:
