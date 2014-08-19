6f46a9099f97928e46fd66f0508bf075292f76b7
Gabor Recski <gabor@cliqz.com>
2014-08-19 20:41:34 +0200

    hacked a mapping from deep cases to surface cases 	modified:   constants.py

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,12 @@ locative_cases = ["SBL", "SUE", "DEL", # ON
                   "ALL", "ADE", "ABL"] # AT
 deep_cases = ['=AGT', '=PAT', '=DAT', '=FROM','=TO', '=POSS', '=LAM'] # "ACC", "DAT", "INS", "OBL"] + ["POSS", "ROOT"]
 
+#should be part of some langspec module, this is just a temporary hack
+deep_case_to_grammatical_case = {
+    '=AGT': 'NOM',
+    '=PAT': 'ACC'
+}
+
 # Special prefixes, separators, etc. that occur in definitions
 avm_pre  = '#'
 deep_pre = '='
