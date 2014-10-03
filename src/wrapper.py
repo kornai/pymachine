409b8e84107386d233c3ffe7bba21d2797c2627c
Gabor Recski <recski@sztaki.hu>
2014-10-03 11:54:07 +0200

    will now load a dependency map and then process output of stanford dep parser

--- src/wrapper.py
+++ src/wrapper.py
@@ -2,21 +2,27 @@
 
 import os
 import pickle
+import re
 import sys
 import logging
 import ConfigParser
 
-from pymachine.src.construction import VerbConstruction
+from construction import VerbConstruction
 from sentence_parser import SentenceParser
 from lexicon import Lexicon
-from machine import MachineGraph
+from pymachine.src.machine import Machine, MachineGraph
+from pymachine.src.control import KRPosControl
 from spreading_activation import SpreadingActivation
 from definition_parser import read as read_defs
 from sup_dic import supplementary_dictionary_reader as sdreader
+from dep_map import dep_map_reader
 #from demo_misc import add_verb_constructions, add_avm_constructions
 import np_grammar
 
 class Wrapper:
+
+    dep_regex = re.compile('([a-z]*)\((.*?)-([0-9]*), (.*?)-([0-9]*)\)')
+
     def __init__(self, cf):
         self.cfn = cf
         self.__read_config()
@@ -34,12 +40,14 @@ class Wrapper:
         items = dict(config.items("machine"))
         self.def_files = [(s.split(":")[0].strip(), int(s.split(":")[1]))
                           for s in items["definitions"].split(",")]
+        self.dep_map_fn = items.get("dep_map")
         self.supp_dict_fn = items.get("supp_dict")
         self.plural_fn = items.get("plurals")
 
     def __read_files(self):
         self.__read_definitions()
         self.__read_supp_dict()
+        self.dep_to_op = dep_map_reader(self.dep_map_fn, self.lexicon)
 
     def __read_definitions(self):
         for file_name, printname_index in self.def_files:
@@ -79,6 +87,20 @@ class Wrapper:
         #add_verb_constructions(self.lexicon, self.supp_dict)
         #add_avm_constructions(self.lexicon, self.supp_dict)
 
+    def add_dependency(self, string):
+        #e.g. nsubjpass(pushed-7, salesman-5)
+        """Given a triplet from Stanford Dep.: D(w1,w2), we create and activate
+        machines for w1 and w2, then run all operators associated with D on the
+        sequence of the new machines (m1, m2)"""
+        dep, word1, id1, word2, id2 = Wrapper.dep_regex.match(string).groups()
+        ana1, ana2 = ('???/UNKNOWN',) * 2
+        machine1 = Machine(word1, KRPosControl(ana1))
+        machine2 = Machine(word2, KRPosControl(ana2))
+        for machine in (machine1, machine2):
+            self.lexicon.add_active(machine)
+        for operator in self.dep_to_op[dep]:
+            operator.act((machine1, machine2))
+
     def run(self, sentence):
         """Parses a sentence, runs the spreading activation and returns the
         messages that have to be sent to the active plugins."""
@@ -104,7 +126,7 @@ class Wrapper:
             print 'results:', results
             print 'machines:', machines
 
-            graph = MachineGraph.create_from_machines(machines)
+            graph = MachineGraph.create_from_machines(machines, max_depth=1)
             f = open('machines.dot', 'w')
             f.write(graph.to_dot())
 
@@ -127,13 +149,27 @@ if __name__ == "__main__":
     #dg = w.lexicon.extract_definition_graph()
     #print dg
     test_sen = [
+        ([
+            ("The", "the/ART"),
+            ("snake", "snake/NOUN")], 'NP'),
+        ("ate", "eat/VERB<PAST>"),
+        ([
+            ("the", "the/ART"),
+            ("elephant", "elephant/NOUN")], 'NP')]
+
+    """
+    test_sen = [
         ([("vets", "vet/NOUN<PLUR>")], 'NP'),
         ("heal", "heal/VERB"),
         ([
             #("sick", "sick/ADJ"),
             ("zebras", "zebra/NOUN<PLUR>")], 'NP')]
+    """
     print 'running...'
-    w.run(test_sen)
+    #w.run(test_sen)
+    for line in sys.stdin:
+        w.add_dependency(line)
     #import pickle
     #pickle.dump(dg, open('foo', 'w'))
     #dg = w.lexicon.extract_definition_graph()
+    #print dg
