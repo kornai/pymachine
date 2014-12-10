7fcf2ec3dcb71f150cf410c71e3990accd246fec
Gabor Recski <recski@sztaki.hu>
2014-12-10 12:32:57 +0100

    a custom version of jaccard that never helps but I still keep thinking it might one day

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,19 @@
 import logging
 
+def min_jaccard(seq1, seq2, log=False):
+    set1, set2 = map(set, (seq1, seq2))
+    shorter_length = min(len(set1), len(set2))
+    intersection = set1 & set2
+    if not intersection:
+        return 0
+    else:
+        sim = float(len(intersection)) / shorter_length
+        if log:
+            logging.info(u'set1: {0}, set2: {1}'.format(set1, set2))
+            logging.info(u'shared: {0}'.format(intersection))
+            logging.info('sim: {0}'.format(sim))
+        return sim
+
 def jaccard(seq1, seq2, log=False):
     set1, set2 = map(set, (seq1, seq2))
     union = set1 | set2
