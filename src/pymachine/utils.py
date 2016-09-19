6a26952aa41f3071351d254a797f851dd5d1d4b5
Eszter Iklodi <eszter.iklodi@gmail.com>
2016-09-19 17:05:25 +0200

    unnecessary line is eliminated

--- src/pymachine/utils.py
+++ src/pymachine/utils.py
@@ -180,8 +180,6 @@ class MachineGraph:
                 node_line = u'\t{0} [shape = circle, label = "{1}"];'.format(
                     d_node, printname).replace('-', '_')
             node_lines.append(node_line)
-            node_lines.append(u'\t{0} [shape = circle, label = "{1}"];'.format(
-                d_node, printname).replace('-', '_'))
         lines += sorted(node_lines)
 
         edge_lines = []
